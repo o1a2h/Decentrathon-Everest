51bed73742e291c32976a709500309019f4c6e7b
o1a2h <bazbazolol188@gmail.com>
2024-10-19 21:36:24 +0500

    2024-10-19-21-36

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,24 +4,71 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform player;
-    private Rigidbody playerRB;
+    public Transform target;
+
+    public Transform playerCar;
+    public List<Transform> AICars;
+
+    public List<GameObject> buttons;
+
+    public int cameraIndex = 0;
+
+    private Rigidbody targetRB;
     public Vector3 Offset;
     public float speed;
+
+    public ButtonController cameraButton;
+    private bool isCameraButtonAlreadyPressed = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        playerRB = player.GetComponent<Rigidbody>();
+        target = playerCar;
+        targetRB = target.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 playerForward = (playerRB.velocity + player.transform.forward).normalized;
+
+        if (cameraButton.isPressed && !isCameraButtonAlreadyPressed)
+        {
+            cameraIndex++;
+            if (cameraIndex == AICars.Count + 1)
+            {
+                cameraIndex = 0;
+            }
+
+            if (cameraIndex == 0)
+            {
+                target = playerCar;
+                foreach(GameObject b in buttons)
+                {
+                    b.SetActive(true);
+                }
+            }
+            else
+            {
+                target = AICars[cameraIndex-1];
+                foreach (GameObject b in buttons)
+                {
+                    b.SetActive(false);
+                }
+            }
+
+            isCameraButtonAlreadyPressed = true;
+        }
+        else if (!cameraButton.isPressed)
+        {
+            isCameraButtonAlreadyPressed = false;
+        }
+
+        Vector3 targetForward = (targetRB.velocity + target.transform.forward).normalized;
         transform.position = Vector3.Lerp(transform.position,
-            player.position + player.transform.TransformVector(Offset)
-            + playerForward * (-5f),
+            target.position + target.transform.TransformVector(Offset)
+            + targetForward * (-5f),
             speed * Time.deltaTime);
-        transform.LookAt(player);
+        transform.LookAt(target);
     }
 }
